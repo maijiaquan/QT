30fae2ff1222f6d0780e48728822a8d2437931b9
Xiaoming Mai <499466765@qq.com>
2016-04-25 22:57:59 +0800

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,8 +9,22 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-    s1.x = 20;
-    s1.y = 20;
+    head->x = 20;
+    head->y = 20;
+
+    snake *s1 = new snake;
+
+    head->next = s1;
+    s1->x = head->x + 40;
+    s1->y = head->y;
+    s1->next = NULL;
+
+    snake *s2 = new snake;
+    s1->next = s2;
+    s2->x = s1->x + 40;
+    s2->y = s1->y;
+    s2->next = NULL;
+
     ui->setupUi(this);
 }
 
@@ -37,20 +51,47 @@ void MainWindow::paintEvent(QPaintEvent *)  //画背景
     brush.setStyle(Qt::SolidPattern);
     brush.setColor(Qt::black);
     p.setBrush(brush);
-    p.drawRect(20,60,40,40);
-    p.drawRect(s1.x,s1.y,40,40);
-}
 
+    snake * temp = new snake;
+    temp = head;
+
+    while(temp != NULL){
+        p.drawRect(temp->x, temp->y, 40, 40);
+        temp = temp->next;
+    }
+
+
+}
 
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
     int keyValue = event->key();
-
     if(keyValue == Qt::Key_Left ){
-        cout<<"left"<<endl;
-        s1.y += 40;
-        cout<< s1.y;
+        head->x -= 40;
         update();
-        cout<< s1.y;
     }
+
+    if(keyValue == Qt::Key_Right){
+        head->x += 40;
+        update();
+    }
+
+    if(keyValue == Qt::Key_Up){
+        head->y -= 40;
+        update();
+    }
+
+    if(keyValue == Qt::Key_Down){
+        head->y += 40;
+        update();
+    }
+}
+
+void MainWindow::moveTail(){
+    int first_x = head->x;
+    int first_y = head->y;
+    int second_x = head->next->x;
+    int second_y = head->next->y;
+
+
 }
