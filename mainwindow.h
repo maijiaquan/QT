11314397dba0dd1d063dfc6ce3243464bd2ca3fb
Xiaoming Mai <499466765@qq.com>
2016-04-30 20:30:44 +0800

    多了定时器的protected声明

--- mainwindow.h
+++ mainwindow.h
@@ -15,18 +15,42 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
-//////////////////////////////////////
+    //////////////////////////////////////
 
     void paintEvent(QPaintEvent *);
     void keyPressEvent(QKeyEvent *);
+
+
+    void moveTail(int a);  //移动蛇尾
+    int foodAhead();  //判断食物是否在前方
+    void moveSnake();
+    void eat();
+
+protected:
+    void timerEvent(QTimerEvent *event);
+
 private:
     Ui::MainWindow *ui;
-    int a[15][15];
 
     struct snake{
         int x;
         int y;
-    }s1;
+        snake *next;
+    };
+    snake *head;
+    snake *food;
+
+    int LINE_WIDTH;
+
+    int TOP_LEFT_X;
+    int TOP_LEFT_Y;
+
+    /*四个方向*/
+    int DIR_UP;
+    int DIR_DOWN;
+    int DIR_LEFT;
+    int DIR_RIGHT;
+    int CURR_DIR;
 };
 
 #endif // MAINWINDOW_H
